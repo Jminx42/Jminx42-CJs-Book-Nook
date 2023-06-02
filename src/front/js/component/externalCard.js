03fcd69ddfe3e816aa9013a445a226175c4fa04e
Jminx42 <105382626+Jminx42@users.noreply.github.com>
2023-06-02 11:36:31 +0000

    fixed toggle, just need to add the functionality where wehn uoi click on the button again it will remove the book from the wishlist/checkout

--- src/front/js/component/externalCard.js
+++ src/front/js/component/externalCard.js
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 import "../../styles/externalCard.css"
+
 // import { library, config } from '@fortawesome/fontawesome-svg-core';
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faHeart as regularHeart } from '@fortawesome/free-regular-svg-icons';
@@ -25,52 +26,63 @@ export const ExternalCard = ({ item }) => {
 
     return (
 
-        <div className="card mx-2 mb-2 p-0" style={{ width: "18rem" }}>
-            <Link to={`/book/${item.primary_isbn13}`}>
+        <div className="card m-2 p-0" style={{ width: "18rem" }}>
+            <Link to={`/book/${item.primary_isbn13}`} className="card-body-custom">
                 <div className="image-container" style={{ height: "320px", overflow: "hidden" }}>
                     <img src={item.book_image} className="card-img-top w-100 h-100" alt="Book Cover" />
                 </div>
-                <div className="card-body text-start">
-                    <div className="row">
-                        <h5 className="card-title">{item.title}</h5>
-                    </div>
+                <div className="card-body text-start ">
+                    <div className="container">
+                        <div className="row">
+                            <h5 className="card-title">
+                                {item.title}
+                            </h5>
+                        </div>
 
-                    <div className="row">
-                        <p className="card-text">{item.author}</p>
+                        <div className="row">
+                            <p className="card-text">{item.author}</p>
+                        </div>
+                        <div className="row">
+                            <p className="card-text"><span className="fw-bold">Price: </span>{item.price}</p>
+                        </div>
                     </div>
-
-                    <div className="row">
-                        <p className="card-text">Price: {item.price}</p>
-                    </div>
-
                 </div>
             </Link >
-            <div className="ms-auto p-1">
+            <div className="card-footer">
+                <div className="">
 
+                    <button
+                        type="button"
+                        className="btn me-2 text-white card-custom-button"
+                        onClick={() => actions.setWishlist(item.primary_isbn13, item.book_image, item.title, item.author)}
+                    >
+                        {store.wishlist.some(
+                            (wishlistItem) =>
+                                wishlistItem.primary_isbn13 === item.primary_isbn13
+                        ) ? (
+                            <i className="fas fa-heart"></i>
+                        ) : (
+                            <i className="far fa-heart"></i>
+                        )}
+                    </button>
 
+                    <button type="button" className="btn text-white card-custom-button"
+                        onClick={() => actions.setCheckout(item.primary_isbn13, item.book_image, item.title, item.author, item.price)}>
+                        {store.checkout.some(
+                            (checkoutItem) =>
+                                checkoutItem.primary_isbn13 === item.primary_isbn13
+                        ) ? (
+                            <i className="bi bi-cart-fill"></i>
+                        ) : (
+                            <i className="bi bi-cart"></i>
+                        )}
 
-                <button
-                    type="button"
-                    className="btn text-white card-custom-button"
-                    onClick={() => actions.setWishlist(item.primary_isbn13, item.book_image, item.title, item.author)}
-                >
-                    {store.wishlist.includes(item.isbn, item.book_image, item.title, item.author) ? (
-                        <i className="fa-solid fa-heart"></i>
-                    ) : (
-                        <i className="fa-regular fa-heart"></i>
-                    )}
-                </button>
-                {/* This button isn't working... it adds to the wishlist correctly but it doesn't toggle between solid and regular */}
-                <button type="button" className="btn text-white card-custom-button ms-2" onClick={() => actions.setCheckout(item.primary_isbn13, item.book_image, item.title, item.author, item.price)}>
-                    <i className="fas fa-shopping-cart"></i>
-                    {/* I want to make this toggle between solid and regular, but need help! */}
-                </button>
+                    </button>
+                </div>
             </div>
-
         </div>
 
     )
         ;
 }
 
-export default ExternalCard
