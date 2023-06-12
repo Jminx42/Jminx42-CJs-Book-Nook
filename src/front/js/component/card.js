e259f7d466f9008fdc73d329c1b3f7d71361a8a0
Carolina Hora <121583492+Carolina-hora@users.noreply.github.com>
2023-06-12 19:33:25 +0000

    profile showing wishlist

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -10,16 +10,20 @@ import "../../styles/home.css";
 export const Card = ({ item }) => {
     const { store, actions } = useContext(Context);
 
+
+
     return (
 
         <div className="card mx-2 border-0" style={{ width: "18rem" }}>
+
             <Link to={`/book/${item.isbn}`}>
-                <img src={item.cover} className="card-img-top" alt="..." />
+                <img src={item.book_cover} className="card-img-top" alt="..." />
             </Link>
+
             <div className="card-body" >
                 <h5 className="card-text text-start">Title: {item.title}</h5>
                 <p className="card-text text-start">Author: {item.author}</p>
-                <p className="card-text text-start">Price: {item.price}</p>
+                <p className="card-text text-start">Average rating: {item.average_rating}</p>
             </div>
         </div>
     );
